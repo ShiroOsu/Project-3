a5f0d54e917d86f0a4231d49bdfb04826a7d307b
ShiroOsu <granen.n@gmail.com>
2017-11-20 22:50:53 +0100

    Update Election.java

--- Task 1/Election.java
+++ Task 1/Election.java
@@ -26,39 +26,42 @@ public class Election{
 	
 	private void getVotes(String file){
 		// first comes democraticVotes then republicanVotes and last independentVotes
-		//oklahoma does not have any independentVotes (state = 36) 
-		int c = 1;
+		//oklahoma does not have any independentVotes
+		int c = 0;
 		int d = 0;
 		boolean found = false;
 		for(int i = -1; (i = file.indexOf("at\">", i + 1)) != -1; i++){
 			int to = file.indexOf("</td", i);
 			
 			String s = file.substring(i + 4, to);
-			
-			// democraticVotes
-			if(c == 1){
-				dVotes.add(s);
-			} 
-			
-			// republicanVotes
-			if(c == 2){
-				rVotes.add(s);
-			}
-			
-			//TODO add iVotes.add("0"); when we reach oklahoma and then jump back to c = 1
-			
-			// independentVotes
-			if(c == 3){
-				iVotes.add(s);
-			}
-			
 			c++;
-			
+			d++;
+	
 			if(c > 3){
 				c = 1;
 			}
 			
+			// democraticVotes
+			if(c == 1){
+				dVotes.add(s);
+			} else if(c == 2){ // republicanVotes
+				rVotes.add(s);
+			}
 			
+			
+			//check if state is oklahoma, loop runs 152 times
+			//d == 110 is when we add republicanVotes to Oklahoma so we need to add 0 to independentVotes
+			//then jump back to start of loop
+			if(d == 110){
+				iVotes.add("0");
+				c = 0;
+			}
+			
+			
+			
+			if(c == 3){ // independentVotes
+				iVotes.add(s);
+			}
 		}
 	}
 	
@@ -71,8 +74,10 @@ public class Election{
 		getStates(text);
 		getVotes(text);
 		
+		//print everything out to console
 		for(int i = 0; i < states.size(); i++){
-			System.out.println("State: " + states.get(i) + " democraticVotes: " + dVotes.get(i) + " republicanVotes: " + rVotes.get(i) + " independentVotes: " + iVotes.get(i));
+			System.out.println("State: " + states.get(i) + " DemocraticVotes: " + dVotes.get(i) + "," + 
+			" RepublicanVotes: " + rVotes.get(i) + "," + " IndependentVotes: " + iVotes.get(i));
 		}
 		
 		
